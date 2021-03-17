7a815c6c195eda33e53912807467665dff3a063a
jsouza <jsouza@grupoimagetech.com.br>
2021-03-17 13:12:13 -0400

    teste inserir usuario

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 const app = require('express')();
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
    res.status(200).send();
@@ -9,6 +12,10 @@ app.get('/users', (req, res) => {
       {name: 'Jhon Doe', mail: 'jhon@mail.com'},
    ]
    res.status(200).json(users)
+})
+
+app.post('/users', (req, res) => {
+   res.status(201).json(req.body)
 })
 
 module.exports = app;
--- test/user.test.js
+++ test/user.test.js
@@ -1,5 +1,7 @@
 const request = require('supertest')
+
 const app = require('../src/app')
+
 test('Deve listar usuarios ', () => {
     return request(app).get('/users')
         .then((res) => {
@@ -7,4 +9,13 @@ test('Deve listar usuarios ', () => {
             expect(res.body).toHaveLength(1);
             expect(res.body[0]).toHaveProperty('name', 'Jhon Doe')
     });
+})
+
+test('Deve inserir usuario com sucesso', () => {
+    return request(app).post('/users')
+        .send({name: 'Walter Mitty', email: 'walter@mail.com'})
+        .then((res) => {
+            expect(res.status).toBe(201)
+            expect(res.body.name).toBe('Walter Mitty')
+        })
 })
